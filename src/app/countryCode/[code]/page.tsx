0ec6e402a6a442e5e87791591c603b1c3d1bc70c
Elahe Narimani <elahe.narimani@gmail.com>
2025-06-11 11:23:10 +0330

    edit

--- src/app/countryCode/[code]/page.tsx
+++ src/app/countryCode/[code]/page.tsx
@@ -1,8 +1,25 @@
-import CountryDetails from "../../../components/countryDetails/CountryDetails";
+// import CountryDetails from "../../../components/countryDetails/CountryDetails";
+// import React from "react";
+// export default function DaynamicPage({ params }: { params: { code: string } }) {
+//   const { code } = params;
+//   console.log("code received:", code);
+//   return (
+//     <div>
+//       <CountryDetails code={code} />
+//     </div>
+//   );
+// }
+
+
+import CountryDetails from "@/components/countryDetails/CountryDetails";
 import React from "react";
-export default function DaynamicPage({ params }: { params: { code: string } }) {
-  const { code } = params;
-  console.log("code received:", code);
+export default async function page({
+  params,
+}: {
+  params: Promise<{ code: string }>;
+}) {
+  const code = await (await params).code;
+  console.log(code)
   return (
     <div>
       <CountryDetails code={code} />
